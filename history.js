bdd42ce8b7f5003e59427d1ca0576e8f9e73b871
RyanConwell <35277965+RyanConwell@users.noreply.github.com>
2018-02-23 15:17:00 -0500

    Fixed iOS's stupid quotemarks
    I have no clue what it was trying to accomplish.

--- history.js
+++ history.js
@@ -1,64 +1,64 @@
-$(‘#quiz’).quiz({
+$('#quiz').quiz({
     questions: [
         {
-            ‘q’: ‘Which of the following led the US into WWII’,
-            ‘options’: [
-                ‘Operation Pastorius’,
-                ‘Fort Stevens and Lookout Air Raids’,
-                ‘Ellwood Oil Field’,
-                ‘Pearl Harbor’
+            'q': 'Which of the following led the US into WWII',
+            'options': [
+                'Operation Pastorius',
+                'Fort Stevens and Lookout Air Raids',
+                'Ellwood Oil Field',
+                'Pearl Harbor'
             ],
-            ‘correctIndex’: 3,
-            ‘correctResponse’: ‘Correct!’,
-            ‘incorrectResponse’: ‘Incorrect!’
+            'correctIndex': 3,
+            'correctResponse': 'Correct!',
+            'incorrectResponse': 'Incorrect!'
         },
         {
-            ‘q’: ‘What did Hitler take over in 1941?’,
-            ‘options’: [
-                ‘The United States’,
-                ‘Parts of Russia and Yugoslavia’,
-                ‘Canada’,
-                ‘Eastern Asia’
+            'q': 'What did Hitler take over in 1941?',
+            'options': [
+                'The United States',
+                'Parts of Russia and Yugoslavia',
+                'Canada',
+                'Eastern Asia'
             ],
-            ‘correctIndex’: 1,
-            ‘correctResponse’: ‘Correct!’,
-            ‘incorrectResponse’: ‘Incorrect!’
+            'correctIndex': 1,
+            'correctResponse': 'Correct!',
+            'incorrectResponse': 'Incorrect!'
         },
         {
-            ‘In what way did the US get involved in WWII?’,
-            ‘options’: [
-                ‘Money’,
-                ‘Provided aid, warships’,
-                ‘Power’,
-                ‘Weapons’
+            'In what way did the US get involved in WWII?',
+            'options': [
+                'Money',
+                'Provided aid, warships',
+                'Power',
+                'Weapons'
             ],
-            ‘correctIndex’: 1,
-            ‘correctResponse’: ‘Correct!’,
-            ‘incorrectResponse’: ‘Incorrect!’
+            'correctIndex': 1,
+            'correctResponse': 'Correct!',
+            'incorrectResponse': 'Incorrect!'
         },
         {
-            ‘q’: ‘When did Germany invade Poland?’,
-            ‘options’: [
-                ‘1st of September, 1939’,
-                ‘9th of May, 1945’,
-                ‘22nd of June, 1941’,
-                ‘31st of October, 1940’
+            'q': 'When did Germany invade Poland?',
+            'options': [
+                '1st of September, 1939',
+                '9th of May, 1945',
+                '22nd of June, 1941',
+                '31st of October, 1940'
             ],
-            ‘correctIndex’: 0,
-            ‘correctResponse’: ‘Correct!’,
-            ‘incorrectResponse’: ‘Incorrect!’
+            'correctIndex': 0,
+            'correctResponse': 'Correct!',
+            'incorrectResponse': 'Incorrect!'
         },
         {
-            ‘q’: ‘Who was the commander of the German Air Force?’,
-            ‘options’: [
-                ‘Adolf Hitler’,
-                ‘Dwight D. Eisenhower’,
-                ‘Hermann Goring’,
-                ‘Chiang Kai-shek’
+            'q': 'Who was the commander of the German Air Force?',
+            'options': [
+                'Adolf Hitler',
+                'Dwight D. Eisenhower',
+                'Hermann Goring',
+                'Chiang Kai-shek'
             ],
-            ‘correctIndex’: 2,
-            ‘correctResponse’: ‘Correct!’,
-            ‘incorrectResponse’: ‘Incorrect!’
+            'correctIndex': 2,
+            'correctResponse': 'Correct!',
+            'incorrectResponse': 'Incorrect!'
         }
     ]
 });
