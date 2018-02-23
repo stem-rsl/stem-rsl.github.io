cd53ff0bc35b2f9d6c93fa8dcad9ca0391d2f386
Ryan Conwell <ryanconwell@protonmail.com>
2018-02-23 16:06:56 -0500

    Added citations, explanations to history section

--- history.js
+++ history.js
@@ -9,8 +9,8 @@ $('#quiz').quiz({
                 'Pearl Harbor'
             ],
             'correctIndex': 3,
-            'correctResponse': 'Correct!',
-            'incorrectResponse': 'Incorrect!'
+            'correctResponse': 'Correct! The attack on Pearl Harbor was a surprise military strike by the Imperial Japanese Navy Air Service against the United States naval base at Pearl Harbor, Hawaii Territory, on the morning of December 7, 1941.<br><i>World War II (1939-1945), www2.gwu.edu/~erpapers/teachinger/glossary/world-war-2.cfm.</i>',
+            'incorrectResponse': 'Incorrect! The attack on Pearl Harbor was a surprise military strike by the Imperial Japanese Navy Air Service against the United States naval base at Pearl Harbor, Hawaii Territory, on the morning of December 7, 1941.'
         },
         {
             'q': 'What did Hitler take over in 1941?',
@@ -21,8 +21,8 @@ $('#quiz').quiz({
                 'Eastern Asia'
             ],
             'correctIndex': 1,
-            'correctResponse': 'Correct!',
-            'incorrectResponse': 'Incorrect!'
+            'correctResponse': 'Correct! Between 1939 and June 1941 the German army invaded and occupied many countries, including the Netherlands, Belgium, Luxembourg, France, Denmark, Yugoslavia, Greece, Norway and Western Poland.<br><i>Fraser, Colin. “The Nazi Invasion of the Balkans & Yugoslavia - A Costly Victory.” WAR HISTORY ONLINE, 29 Aug. 2017, m.warhistoryonline.com/world-war-ii/nazi-invasion-greece- yugoslavia.html.</i>',
+            'incorrectResponse': 'Incorrect! Between 1939 and June 1941 the German army invaded and occupied many countries, including the Netherlands, Belgium, Luxembourg, France, Denmark, Yugoslavia, Greece, Norway and Western Poland.<br><i>Fraser, Colin. “The Nazi Invasion of the Balkans & Yugoslavia - A Costly Victory.” WAR HISTORY ONLINE, 29 Aug. 2017, m.warhistoryonline.com/world-war-ii/nazi-invasion-greece- yugoslavia.html.</i>'
         },
         {
             'q': 'In what way did the US get involved in WWII?',
@@ -33,11 +33,11 @@ $('#quiz').quiz({
                 'Weapons'
             ],
             'correctIndex': 1,
-            'correctResponse': 'Correct!',
-            'incorrectResponse': 'Incorrect!'
+            'correctResponse': 'Correct! America\'s industrial strength, coupled with its able, willing and resolute manpower, propelled the Allies to victory in the Grand Conflict - becoming one of the major players alongside Britain and the Soviet Union in helping to bring the war to a close during 1945.<br><i>U.S. Department of State, U.S. Department of State, history.state.gov/milestones/1937-1945/ lend-lease.</i>' ,
+            'incorrectResponse': 'Incorrect! America\'s industrial strength, coupled with its able, willing and resolute manpower, propelled the Allies to victory in the Grand Conflict - becoming one of the major players alongside Britain and the Soviet Union in helping to bring the war to a close during 1945.<br><i>U.S. Department of State, U.S. Department of State, history.state.gov/milestones/1937-1945/ lend-lease.</i>'
         },
         {
-            'q': 'When did Germany invade Poland?',
+            'q': 'When did the USSR declare war on Germany?',
             'options': [
                 '1st of September, 1939',
                 '9th of May, 1945',
@@ -45,8 +45,8 @@ $('#quiz').quiz({
                 '31st of October, 1940'
             ],
             'correctIndex': 0,
-            'correctResponse': 'Correct!',
-            'incorrectResponse': 'Incorrect!'
+            'correctResponse': 'Correct! The USSR had declared war on Germany on September 3, 1939.<br><i>Nosowska, Agnieszka. “HISTORYOF ENRS.” 17 September 1939 – the Soviet invasion of Poland, www.enrs.eu/en/news/882-17-september-1939-the-soviet-invasion-of-poland.</i>',
+            'incorrectResponse': 'Incorrect! The USSE had declared war on Germany on September 3, 1939.<br><i>Nosowska, Agnieszka. “HISTORYOF ENRS.” 17 September 1939 – the Soviet invasion of Poland, www.enrs.eu/en/news/882-17-september-1939-the-soviet-invasion-of-poland.</i>'
         },
         {
             'q': 'Who was the commander of the German Air Force?',
@@ -57,8 +57,8 @@ $('#quiz').quiz({
                 'Chiang Kai-shek'
             ],
             'correctIndex': 2,
-            'correctResponse': 'Correct!',
-            'incorrectResponse': 'Incorrect!'
+            'correctResponse': 'Correct! The first was Hermann Göring commander of the German Air Force<br><i>“Luftwaffe.” Wikipedia, Wikimedia Foundation, 20 Feb. 2018, en.m.wikipedia.org/wiki/Luftwaffe.</i>',
+            'incorrectResponse': 'Incorrect! The first was Hermann Göring commander of the German Air Force<br><i>“Luftwaffe.” Wikipedia, Wikimedia Foundation, 20 Feb. 2018, en.m.wikipedia.org/wiki/Luftwaffe.</i>'
         }
     ]
 });
